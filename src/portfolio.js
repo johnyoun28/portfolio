9d9f83eb0234e3553b83532b2efbf0f86dabf22a
John Youn <johnyoun28@gmail.com>
2021-10-22 20:29:53 -0700

    added more projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -33,8 +33,28 @@ const projects = [
         description: "Weather application that allows users to view a 5-Day Weather Forecast of their desired location. I've built this project while learning/practicing the basics of Redux!",
         stack: ["HTML", "CSS", "JavaScript", "React", "Redux"],
         github: "https://github.com/johnyoun28/weather-app",
-        live: "github.com/johnyoun28"
+        live: "https://weathers-app.vercel.app/"
     },
+
+    {
+        name: "Secret Family Recipes",
+        description: "An application that allows users to store their own recipes, or view recipes that others have shared. I contributed on the front end UI marketing page with HTML and CSS to inform new users on what the application does",
+        stack: ["HTML", "CSS"],
+        github: "https://github.com/Secret-Family-Recipes-2-BW/SFR2-UI",
+        live: "https://secret-family-recipes2-ui.netlify.app/index.html"
+    },
+
+    {
+        name: "Expat Journal",
+        description: "An application that allows users to share posts or photos for others to view and interact with. I contributed to the back end of this project and used Node.js to implement authentication for users to sign in and sign up.",
+        stack: ["Node.js", "Express", "PostgresSQL"],
+        github: "https://github.com/BW-Expat-Journal-Project/backend",
+        live: "https://github.com/BW-Expat-Journal-Project/backend"
+    },
+    
+
+    
+
 ]
 
 const skills = [ "HTML", "CSS", "JavaScript", "React", "Redux", "React Testing Library", "Cypress", "Node.js", "Express", "PostgresSQL", "Python", "Git", "GitHub"]
