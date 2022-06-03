5099797986a4208fccc9acd91aa3b02cd2cd2408
johnyoun <johnyoun28@gmail.com>
2022-06-02 23:06:12 -0700

    add fade animation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+import { useEffect } from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar'
 import About from './Components/About/About'
 import Projects from './Components/Projects/Projects';
 import Skills from './Components/Skills/Skills'
 import Contact from './Components/Contact/Contact'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+
+
+
 function App() {
+
+  useEffect(()=> {
+    Aos.init({duration:2000})
+  },[])
+
   return (
     <div className="App">
     <NavBar />
     <About />
+    <div data-aos='fade-left'>
     <Projects />
-    <Skills />
+    </div>
+    <div data-aos='fade-right'>
+      <Skills />
+    </div>
     <Contact />
     </div>
   );
